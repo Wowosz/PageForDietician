63c211c979b9226dbd50a934d3280efa1d2d7f98
Wowosz <wojciech.szodaa@gmail.com>
2020-07-05 11:14:51 +0200

    modified index page

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,6 @@ import React from "react";
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 import "../styles/header.css";
 import "../styles/icofont/icofont.css";
-import "../styles/telephone/telephone.css"
 
 
 export default function Layout({ children }) {
@@ -15,8 +14,9 @@ export default function Layout({ children }) {
                 </AniLink>
             </div>
         </header>
-            <div className="">
+            <div>
                 <div className="d-flex navi justify-content-center">
+                    
                     <AniLink className="px-2 py-3 text-decoration-none text-white" duration={0.5} hex="#FFFFFF" swipe direction="left" entryOffset={80} to="/">
                         Start
                     </AniLink>
@@ -39,6 +39,10 @@ export default function Layout({ children }) {
                     <AniLink className="px-2 py-3 text-decoration-none text-white" duration={0.5} hex="#FFFFFF" swipe direction="left" entryOffset={80} to="/en">
                         ENG
                     </AniLink>
+                </div>
+                <div className="socialMedia p-2">
+                    <a className="d-inline-block text-decoration-none text-white" href="https://www.instagram.com/ewafilip_dietetyk/?hl=pl"><h3><i class="icofont-instagram"></i></h3></a>
+                    <a className="d-inline-block text-decoration-none text-white pl-1" href="https://www.facebook.com/Ewa-Filip-Dietetyk-100454951498263/?modal=admin_todo_tour"><h3><i class="icofont-facebook"></i></h3></a>
                 </div>
                 </div>
             </div>
